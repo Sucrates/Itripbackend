53e3d32a0348ce9ac33dad682a85ac4200919d94
Su_crates <Su_crates@outlook.com>
2018-04-25 17:00:43 +0800

    授权模块auth完成 biz完成接口 查询国内、国外热门城市、商圈

---
+++ itripbeans/src/main/java/com/ytzl/itrip/beans/vo/ItripAreaDicVO.java
@@ -0,0 +1,28 @@
+package com.ytzl.itrip.beans.vo;
+
+/**
+ * Created by Su_crates on 2018/4/25.
+ */
+public class ItripAreaDicVO {
+
+    //主键
+    private Long id;
+    //区域名称
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
